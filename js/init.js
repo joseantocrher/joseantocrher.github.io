2ba8a3857b9ba703c8906e34e259d6e180bb4e89
joseantocrher <87164872+joseantocrher@users.noreply.github.com>
2021-07-11 20:27:17 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyAXHRomVUOwtDN-Q85rG6oTgknItfFU-d8",
+    authDomain: "chat41h.firebaseapp.com",
+    projectId: "chat41h",
+    storageBucket: "chat41h.appspot.com",
+    messagingSenderId: "27366034098",
+    appId: "1:27366034098:web:0c5f90072b9f7cedf017ab",
+    measurementId: "G-HGKL9C6LV0"
+    });
